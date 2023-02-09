0ac8484d4aa5742c803496cc02aef7467ee8330f
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2023-02-09 14:31:32 +0100

    Created app.listen, reservationsRouter and router.use(reservationsRouter)

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const path = require("path");
 
 const mealsRouter = require("./api/meals");
+const reservationsRouter = require("./api/reservations");
+
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 router.use("/meals", mealsRouter);
+router.use("/reservations", reservationsRouter)
 
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
@@ -31,5 +34,10 @@ if (process.env.API_PATH) {
 app.use("*", (req, res) => {
   res.sendFile(path.join(`${buildPath}/index.html`));
 });
+
+// To listen to port
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
 
 module.exports = app;
