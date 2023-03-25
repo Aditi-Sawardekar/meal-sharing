f96da200dc53f6788a00a96a88ffd4d6bf6a7efd
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2023-03-25 16:59:02 +0100

    Created and render meals.

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import Header from "./Header";
+import MealsList from "./MealsList";
+
 
 function App() {
+  // State while initial fetch
+  const [meals, setMeals] = useState([]);
+  
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch("api/meals");
+        const menuItems = await response.json();
+        
+        setMeals(menuItems);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    (async () => await fetchMeals())();
+  }, []);
+
   return (
+    <div>
+      <Header title="Meal Sharing" />
+      <MealsList 
+        meals={meals}
+      />
+   
+   
+
+    //Already in file, commented and saved if required in future
     <Router>
       <Route exact path="/">
-        <p>test</p>
+        <p>testing React </p>
       </Route>
       <Route exact path="/lol">
         <p>lol</p>
@@ -15,6 +44,8 @@ function App() {
         <TestComponent></TestComponent>
       </Route>
     </Router>
+    </div>
+  
   );
 }
 
---
+++ src/client/Meal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Meal({ meal }) {
+    console.log(meal)
+  return (
+    <li>
+        <h3>{meal.title}</h3>
+        <p>{meal.description}</p>
+        <h4>{meal.location}</h4>
+        <p>Price: {meal.price} Dkk</p>
+
+    </li>
+    
+  )
+}
+
+export default Meal
